3b998650f3ebdaef98e8897f07c1e939fafe18a8
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-10-06 18:52:58 -0500

    imported logger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import logger from 'redux-logger';
 
 ReactDOM.render(
     <Provider store={storeInstance}>
