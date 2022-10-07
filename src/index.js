1fd3d501f1eda54eade160a20abebe7865d8a187
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-10-06 19:10:43 -0500

    imported store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
 
 ReactDOM.render(
     <Provider store={storeInstance}>
